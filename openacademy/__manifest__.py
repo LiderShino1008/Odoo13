01c6b66c75c8fa3d1ee5021f052061958d3c54cd
LiderShino1008 <walterghost@live.com>
2020-07-23 13:54:58 -0600

    ADD Wizard

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -31,6 +31,7 @@
         'views/course.xml',
         'views/session.xml',
         'views/partner.xml',
+        'views/wizard.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
