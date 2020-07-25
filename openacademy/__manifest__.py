c96e4e1b0568db787ab4727fdbaddb4b1afa8d89
LiderShino1008 <walterghost@live.com>
2020-07-24 23:36:47 -0600

    ADD Reporting

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -32,6 +32,8 @@
         'views/session.xml',
         'views/partner.xml',
         'views/wizard.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
